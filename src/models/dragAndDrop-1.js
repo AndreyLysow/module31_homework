ab3e082bb96e9592e89d39b20d5855e2faa85840
Andy75 <lysow@yandex.ru>
2023-09-18 07:00:03 +0700

    add dradAndDrop-1.js

---
+++ src/models/dragAndDrop-1.js
@@ -0,0 +1,47 @@
+import $ from "jquery";
+
+// Функция для инициализации перетаскивания элементов
+function initDragAndDrop() {
+  const draggables = document.querySelectorAll(".draggable");
+  const containers = document.querySelectorAll(".task-list");
+
+  // Функция для определения элемента, перед которым нужно вставить перетаскиваемый элемент
+  function getDragAfterElement(container, y) {
+    const draggableElements = Array.from(container.querySelectorAll(".draggable:not(.dragging)"));
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      } else {
+        return closest;
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+  }
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+      draggable.classList.add("dragging");
+    });
+
+    draggable.addEventListener("dragend", () => {
+      draggable.classList.remove("dragging");
+    });
+  });
+
+  containers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientY);
+      const draggable = document.querySelector(".dragging");
+      if (draggable && afterElement == null) {
+        container.appendChild(draggable);
+      } else if (draggable) {
+        container.insertBefore(draggable, afterElement);
+      }
+    });
+  });
+}
+
+export { initDragAndDrop };
